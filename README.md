ba8fd8cabaceb650758b52391b42ac4d1c914f9b
Clémence des Mazery <Clemence@MacBook-Air-de-Clemence-3.local>
2024-01-29 17:01:27 +0100

    g_save_mac_Clem

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cpp00_cpp04
+
+When should I use reference / pointer ?
+https://isocpp.org/wiki/faq/references#:~:text=Use%20references%20when%20you%20can,and%20pointers%20on%20the%20inside.
+
+files manipulation in cpp
+https://www.mygreatlearning.com/blog/file-handling-in-cpp/
---
+++ cpp_01/ex04/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+std::string replaceString(const std::string& line, const std::string& s1, const std::string& s2)
+{
+	std::string result = line;
+	size_t pos = 0;
+	while ((pos = result.find(s1, pos)) != std::string::npos) {
+		result.replace(pos, s1.length(), s2);
+		pos += s2.length();
+	}
+	return result;
+}
+
+
+int	main(int ac, char **av)
+{
+	std::fstream	inFile;
+	std::fstream	outFile;
+	std::string		fileName;
+	std::string		s1;
+	std::string		s2;
+	std::string		line;
+
+	if (ac != 4)
+	{
+		std::cout << "Wrong number of arguments.\nExpected format: [filename][string to be replaced][string of replacement]." << std::endl;
+		return (1);
+	}
+
+	fileName = av[1];
+	s1 = av[2];
+	s2 = av[3];
+
+	inFile.open(fileName, std::ios::in);
+	if (!inFile.is_open())
+	{
+		std::cout << "Error opening " << fileName << "." << std::endl;
+		return (1);
+	}
+	outFile.open(fileName + ".replace", std::ios::out);
+	if (!outFile.is_open())
+	{
+		std::cout << "Error creating " << fileName << ".replace." << std::endl;
+		return (1);
+	}
+	while (std::getline(inFile, line))
+	{
+		std::string replacedLine = replaceString(line, s1, s2);
+		outFile << replacedLine << std::endl;
+	}
+
+	inFile.close();
+	outFile.close();
+
+	std::cout << "File " << fileName << " successfully replaced." << std::endl;
+	return (0);
+}
