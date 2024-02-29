1a9e75f58e92754a8d63b4f5980770be124a4d61
lrsl <79993112+lrsl@users.noreply.github.com>
2024-02-29 12:27:09 +0100

    gsave_predator_linux

---
+++ cpp_08/ex00/easyfind.hpp
@@ -0,0 +1,30 @@
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+#include <exception>
+
+class NotFoundException: public std::exception
+{
+	public:
+			virtual const char *what() const throw()
+			{
+				return ("Not found");
+			}
+};
+
+template< typename T>
+typename T::iterator easyfind(T &in, int i)
+{
+	typename T::iterator	it;
+	it = find(in.begin(), in.end(), i);
+	if (it == in.end())
+	{
+		throw (NotFoundException());
+	}
+	return (it);
+}
+
+#endif
