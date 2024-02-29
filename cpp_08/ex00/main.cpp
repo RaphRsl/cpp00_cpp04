292ba57dcd26f9ff7e0c1cdbba2e1e98d844f718
Raphael ROUSSEL <rroussel@bess-f3r5s13.clusters.42paris.fr>
2024-02-29 17:10:00 +0100

    g_save_42

--- cpp_08/ex00/main.cpp
+++ cpp_08/ex00/main.cpp
@@ -3,67 +3,131 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rsl <rsl@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: rroussel <rroussel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 14:36:20 by tblaase           #+#    #+#             */
-/*   Updated: 2024/02/29 13:24:34 by rsl              ###   ########.fr       */
+/*   Updated: 2024/02/29 14:52:11 by rroussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 #include <list>
+#include <vector>
 
-int main()
+void test_list(void)
 {
-	std::list<int> test_list;
-	test_list.push_back(42);
-	test_list.push_back(43);
-	test_list.push_back(55);
+	std::list<int> testList;
+	testList.push_back(42);
+	testList.push_back(43);
+	testList.push_back(55);
 	
-	std::list<int>::const_iterator	it = test_list.end();
+	std::list<int>::const_iterator	it = testList.end();
 
 	std::cout << "===== Looking for 42 =====" << std::endl;
 	try
 	{
-		it = ::easyfind(test_list, 42);
+		it = ::easyfind(testList, 42);
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << std::endl;
 	}
 
-	if (it != test_list.end())
+	if (it != testList.end())
 		std::cout << "\t-> " << *it << " found" << std::endl;
 
-	it = test_list.end();
+	it = testList.end();
 
 	std::cout << "===== Looking for 55 =====" << std::endl;
 	try
 	{
-		it = ::easyfind(test_list, 55);
+		it = ::easyfind(testList, 55);
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << std::endl;
 	}
 
-	if (it != test_list.end())
+	if (it != testList.end())
 		std::cout << "\t-> " << *it << " found" << std::endl;
 
-	it = test_list.end();
+	it = testList.end();
 
 	std::cout << "===== Looking for 52 =====" << std::endl;
 	try
 	{
-		it = ::easyfind(test_list, 52);
+		it = ::easyfind(testList, 52);
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << std::endl;
 	}
 
-	if (it != test_list.end())
+	if (it != testList.end())
+		std::cout << "\t-> " << *it << " found" << std::endl;
+	return ;
+}
+
+
+void	test_vector(void)
+{
+	std::vector<char> testVector;
+	testVector.push_back('T');
+	testVector.push_back('e');
+	testVector.push_back('s');
+	testVector.push_back('t');
+	testVector.push_back(' ');
+	testVector.push_back('i');
+	testVector.push_back('s');
+	testVector.push_back(' ');
+	testVector.push_back('O');
+	testVector.push_back('K');
+	testVector.push_back('!');
+
+	std::cout << "===== Printing my entire vector =====" << std::endl;
+	std::cout << "\tVector: " << std::flush;
+	for(size_t i = 0; i < testVector.size(); i++)
+	    std::cout << testVector[i] << std::flush;
+	std::cout << std::endl;
+	
+	std::vector<char>::const_iterator	it = testVector.end();
+
+	std::cout << "===== Looking for K =====" << std::endl;
+	try
+	{
+		it = ::easyfind(testVector, 'K');
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	if (it != testVector.end())
 		std::cout << "\t-> " << *it << " found" << std::endl;
 
+	it = testVector.end();
+
+	std::cout << "===== Looking for N =====" << std::endl;
+	try
+	{
+		it = ::easyfind(testVector, 'N');
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	if (it != testVector.end())
+		std::cout << "\t-> " << *it << " found" << std::endl;
+	return ;
+}
+
+int main()
+{
+	std::cout << " <<<<< TEST LIST >>>>>" << std::endl;
+	test_list();
+	std::cout << std::endl;
+	std::cout << " <<<<< TEST VECTOR >>>>>" << std::endl;
+	test_vector();
 	return (0);
 }
